dda59354d5cb1ab0dd28a8e516e72081633816ec
Michael Salas <michaelsalasalcedo15@gmail.com>
2023-09-18 15:12:55 -0500

    environment variable configuration

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 module.exports = {
-    user: 'sa',
-    password: 'app12345#',
-    server: '172.17.176.1',
-    database: 'CustomerInfoDB',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server:  process.env.DB_SERVER,
+    database: process.env.DB_NAME,
     options: {
         encrypt: false,
     },
