393e106b08fcda7e6debfc21170f2a1381f9cc3e
Michael Salas <michaelsalasalcedo15@gmail.com>
2023-09-19 00:04:51 -0500

    services to update and delete a customer

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -3,6 +3,28 @@ const mssql = require('mssql');
 const dbConfig = require('../config/dbconfig');
 const { errorResponseFormat, successResponseFormat} = require("../functions/responseFunctions");
 
+
+exports.getAll = async(req, res) => {
+
+    try {
+
+        const pool = await mssql.connect(dbConfig);
+        const request = pool.request();
+    
+        request.input('Operation', mssql.NVarChar(10), 'SELECTALL');
+        const result = await request.execute('sp_Customers_CRUD');
+
+        res
+        .status(200)
+        .send(successResponseFormat(result.recordset[0]));
+  
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(errorResponseFormat('An error occurred'));
+    }
+}
+
 exports.save = async (req, res) => {
     
     try {
@@ -15,7 +37,6 @@ exports.save = async (req, res) => {
       // Definir los parámetros del procedimiento almacenado
       const operation = 'INSERT';
       request.input('Operation', mssql.NVarChar(10), operation);
-      request.input('CustomerID', mssql.Int, 1);
       request.input('IdentificationTypeID', mssql.Int, identificationTypeID);
       request.input('IdentificationNumber', mssql.NVarChar(50), identificationNumber);
       request.input('Name', mssql.NVarChar(50), name);
@@ -34,6 +55,62 @@ exports.save = async (req, res) => {
     }
   };
 
+  exports.update = async (req, res) => {
+   
+    try {
+        const { customerID, identificationTypeID, identificationNumber,name,gender,imageURL } = req.body;
+        
+        const pool = await mssql.connect(dbConfig);
+        const request = pool.request();
+
+        request.input('Operation', mssql.NVarChar(10), 'UPDATE');
+        request.input('CustomerID', mssql.Int, customerID);
+        request.input('IdentificationTypeID', mssql.Int, identificationTypeID);
+        request.input('IdentificationNumber', mssql.NVarChar(50), identificationNumber);
+        request.input('Name', mssql.NVarChar(50), name);
+        request.input('Gender', mssql.NVarChar(10), gender);
+        request.input('ImageURL', mssql.NVarChar(255), imageURL);
+
+        await request.execute('sp_Customers_CRUD');
+        console.log('Cliente actualizado:', result.recordset[0]);
+        
+        res
+        .status(200)
+        .send(successResponseFormat(result.recordset[0]));
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(errorResponseFormat('An error occurred'));
+    }
+
+  }
+
+  exports.delete = async (req, res) => {
+   
+    try {
+
+        const { customerID } = req.body;
+  
+        const pool = await mssql.connect(dbConfig);
+        const request = pool.request();
+
+        request.input('Operation', mssql.NVarChar(10), 'DELETE');
+        request.input('CustomerID', mssql.Int, customerID);
+
+        await request.execute('sp_Customers_CRUD');
+        console.log('Cliente eliminado:', result.recordset[0]);
+        
+        res
+        .status(200)
+        .send(successResponseFormat(result.recordset[0]));
+  
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(errorResponseFormat('An error occurred'));
+    }
+
+  }
 
  
   
--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -3,6 +3,9 @@ const customerController = require("../controllers/customer.controller");
 
 const app = express();
 
+app.get("/customers",customerController.getAll);
 app.post("/create",customerController.save);
+app.put("/update",customerController.update);
+app.delete("/delete",customerController.delete);
 
 module.exports = app;
